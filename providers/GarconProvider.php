22a3d96e8b99ae368a42b7b6a35c99cb43d902b2
Renich Bon Ciric <renich@woralelandia.com>
2011-08-27 20:02:50 -0500

    creé el provider garcon para instalar y desinstalar

---
+++ providers/GarconProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'Zend/Tool/Project/Provider/Abstract.php';
+require_once 'Zend/Tool/Project/Provider/Exception.php';
+
+class GarconProvider extends Zend_Tool_Project_Provider_Abstract
+{
+    public function instalar()
+    {
+        /** @todo Implementation */
+    }
+
+    public function desinstalar()
+    {
+        /** @todo Implementation */
+    }
+}
