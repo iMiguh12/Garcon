adf18e48cb669c1f95075eaa476280bc0c1832b2
OmarRLO <omarloren@gmail.com>
2011-10-02 16:59:09 -0500

    agrego JavaScript para compra de productos

--- public/js/garcon.js
+++ public/js/garcon.js
@@ -4,11 +4,14 @@
 $(document).ready(function(){
 	var array = new Array();
 	var cantProd;	
+          var totalOrden=0;
 	$('.compra').click(function(){
-		var idProd = $(this).parent().attr("id");
-		var nomProd = $(this).parent().find(".nombre").html();
+		var idProd = $(this).parent().parent().attr("id");
+                    var nomProd = $(this).parent().find(".nombre").html();
 		var precProd = $(this).parent().find(".precio").html();
-				
+                   //acumulamos el precio para sacar el total
+                   totalOrden= totalOrden + (parseInt(precProd));
+                    
 		if ( buscarProd(array,idProd) ){
 			//Si ya existe sumamos el valor
 			
@@ -16,11 +19,14 @@ $(document).ready(function(){
 			var sumProd =  precProd*cantProd;
 			$('#prod-'+idProd).children(".nombre").find('.precio').html(sumProd);
 			$('#prod-'+idProd).children(".nombre").find('em').html(cantProd);
-			
+			$('#total').html(totalOrden);
 			
 		}else{
 			array.push(idProd);
-			$('#prod-selec ul').append("<li id=\"prod-" + idProd + "\"><p class=\"nombre\"> <em>1</em> "+ nomProd + "---> $<span class=\"precio\">"+ precProd + "</span></p></li>");
+			$('#prod-select ul').append("<li id=\"prod-" + idProd + "\"><p class=\"nombre\"> <em>1</em> "
+                                                                + nomProd + "---> $<span class=\"precio\">"+ precProd + "</span></p></li>");
+                               $('#total').html(totalOrden);
+                                             
 		}
 	});
 });
@@ -31,10 +37,8 @@ function buscarProd(arr, id) {
 	  for(var i=0; i<arr.length; i++) {
 	   	if(arr[i]==id){
 	   		return true;
-	   	}else{
-            return false;
-        }
-        
-	  }
+                              break;
+	   	}
+        	  }
 	}
 
