4aaf4748becebaa061fa23a93f7f07f27315d712
armando gomez <armandojpr@yahoo.com>
2011-10-31 19:42:12 -0600

    Se agregan unos archivos que se me olvido agregar en el commit anterior respecto al crud de usuarios

---
+++ application/forms/Usuarios.php
@@ -0,0 +1,57 @@
+<?php
+
+class Application_Form_Usuarios extends Zend_Form
+{
+    public function init()
+    {
+        $this->setName( 'usuario' );
+        $this->setAttrib( Zend_Form::ENCTYPE_MULTIPART, true );
+
+        $id = new Zend_Form_Element_Hidden( 'id' );
+        $id->addFilter( 'Int' );
+
+        $nombre = new Zend_Form_Element_Text( 'nombre' );
+        $nombre->setLabel( 'Nombre' )
+               ->setRequired( 'true' )
+               ->addFilter( 'StripTags' )
+               ->addFilter( 'StringTrim' )
+               ->addValidator( 'NotEmpty' )
+               ->addValidator( 'Regex', false, array( '#^([a-z A-Z0-9ñÑáéíóúÁÉÍÓÚüÜ\(\)-]+$.*)#' ) )
+               ->addValidator( 'stringLength', true, array(1, 255));
+
+        $estados = new Zend_Form_Element_Select('estados');
+        $estados->setLabel('Estado')
+                ->setRequired(true);
+        
+        $usuarios = new Application_Model_DbTable_Usuarios();
+        $meta = $usuarios->getEnumValues( 'estado' );
+
+        foreach($meta as $cat){
+            $estados->addMultiOption($cat, $cat);
+        }
+
+        $email = new Zend_Form_Element_Text( 'email' );
+        $email->setLabel( 'Email' )
+              ->setRequired( 'true' )
+              ->addFilter( 'StripTags' )
+              ->addFilter( 'StringTrim' )
+              ->addValidator( 'NotEmpty' )
+              ->addValidator( 'EmailAddress' )
+              ->addValidator( 'stringLength', true, array( 1, 255 ) );
+                
+        $telefono = new Zend_Form_Element_Text( 'telefono' );
+        $telefono->setLabel( 'Telefono' )
+                 ->setRequired( 'true' )
+                 ->addFilter( 'StripTags' )
+                 ->addFilter( 'StringTrim' )
+                 ->addValidator( 'NotEmpty' )
+                 ->addValidator( 'Regex', false, array( '/^[+]?[-\d() .]*$/i' ) ) ;
+
+        $enviar = new Zend_Form_Element_Submit( 'enviar' );
+        $enviar->setAttrib( 'id', 'botonEnviar' );
+
+        $this->addElements( 
+            array( $id, $nombre, $estados, $email, $telefono, $enviar ) 
+        );
+    }
+}
