9b03675d9f22064ce7456c91fbca9dce507743db
Renich Bon Ciric <renich@woralelandia.com>
2011-11-26 21:24:16 -0600

    primer intento de poner links de edit y delete para productos
    No me salió; debo corregir los links

--- application/views/helpers/DisplayProducto.php
+++ application/views/helpers/DisplayProducto.php
@@ -2,20 +2,12 @@
 
 class Zend_View_Helper_DisplayProducto extends Zend_View_Helper_Abstract 
 {
-    public function displayProducto( $mime, $image, $id = null, $class = null, $alt = null, $title = '', $price = null, $description = null )
+    public function displayProducto( $mime, $image, $alt = null, $title = '' )
     {
         $image = base64_encode( $image );
 
     	$data_uri = ( 'data:' . $mime . ';base64,' . $image );
 
-    	return <<<EOF
-<figure class='imagen' id='producto-{$id}'>    
-    <img src='$data_uri' alt='$alt' title='$title' />
-    <figcaption class='edit'>edit</figcaption>
-    <figcaption class='delete'>delete</figcaption>
-    <figcaption class='precio'>{$price}</figcaption>
-    <figcaption class='descripcion'>{$description}</figcaption>
-</figure>
-EOF;
+    	return "<img src='$data_uri' alt='$alt' title='$title' />";
     }
 }
