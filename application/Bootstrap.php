7fc6b2276fa0344fa15009e2b113cf2b346b7db3
Renich Bon Ciric <renich@woralelandia.com>
2012-04-20 21:11:51 -0500

    code cleanup y refactoring de plugins
    Puse la carpeta de Plugins bajo nuestro directorio Garcon; en library.
    
    Debemos buscar cual es la manera adecuada de organizar nuestros archivos en ZendFramework

--- application/Bootstrap.php
+++ application/Bootstrap.php
@@ -9,16 +9,16 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
     }
     
     protected function _initAutoload()
-	{
-		$moduleLoader = new Zend_Application_Module_Autoloader(array(
-			'namespace' => '',
-			'basePath' => APPLICATION_PATH));
+    {
+        $moduleLoader = new Zend_Application_Module_Autoloader(array(
+            'namespace' => '',
+            'basePath' => APPLICATION_PATH));
 
-		$autoloader = Zend_Loader_Autoloader::getInstance();
-		$autoloader->registerNamespace(array('Plugins_'));
-		
-		return $moduleLoader;		
-	}
+        $autoloader = Zend_Loader_Autoloader::getInstance();
+        $autoloader->registerNamespace(array('Plugins_'));
+        
+        return $moduleLoader;       
+    }
 
     // View
     protected function _initView()
@@ -101,45 +101,43 @@ class Bootstrap extends Zend_Application_Bootstrap_Bootstrap
     // configuración de las ACLs
     protected function _initAcl()
     {
-    	$acl = new Zend_Acl();
-    	
-    	$acl->addRole( 'invitado' );
-    	$acl->addRole( 'usuario' );
-    	$acl->addRole( 'administrador' );
-		
-    	$acl->add( new Zend_Acl_Resource( 'admin:productos' ) );
-    	$acl->add( new Zend_Acl_Resource( 'admin:usuarios' ) );
-		
-    	$acl->allow('administrador', 'admin:productos' );
-    	$acl->allow('administrador', 'admin:usuarios' );
-    	
-    	
-    	//A continuacion se pondria las urls (como un resource cada una) que se quieren proteger para que usuarios que 
-    	//no tienen el permiso no puedan entrar, por lo pronto nomas puse una
-    	$acl->add( new Zend_Acl_Resource( 'movimientos/adquirir' ) );
-    	
-    	//Solamente los usuarios con el rol de administrador podran entrar a la direccion http://<contextoDeGarcon>/movimientos/adquirir
-    	$acl->allow('administrador', 'movimientos/adquirir');
-    	
-    	//ponemos el acl en Zend_Registry
-    	Zend_Registry::set('acl',$acl);
-    	
-    	
-    	// Store ACL and role in the proxy helper
-    	$view = $this->view;
-    	//Si no se ha logueado, se asigna por default el rol de invitado
-    	$autentificacion = Zend_Auth::getInstance();    
-    	$rol = 'invitado';
-    	if($autentificacion->hasIdentity() ){
-    		$rol = $autentificacion->getIdentity()->rol;
-    	}
-    	
-    	
-    	$view->navigation()->setAcl($acl)->setRole( $rol );
-    	
-    	//registramos el plugin que autentifica si el usuario puede ver la url
-    	$controller = Zend_Controller_Front::getInstance();		
-	$controller->registerPlugin(new Plugins_AuthPlugin());
+        $acl = new Zend_Acl();
+        
+        $acl->addRole( 'invitado' );
+        $acl->addRole( 'usuario' );
+        $acl->addRole( 'administrador' );
+        
+        $acl->add( new Zend_Acl_Resource( 'admin:productos' ) );
+        $acl->add( new Zend_Acl_Resource( 'admin:usuarios' ) );
+        
+        $acl->allow('administrador', 'admin:productos' );
+        $acl->allow('administrador', 'admin:usuarios' );
+        
+        // A continuacion se pondria las urls (como un resource cada una) que se quieren proteger para que usuarios que 
+        // no tienen el permiso no puedan entrar, por lo pronto nomas puse una
+        $acl->add( new Zend_Acl_Resource( 'movimientos/adquirir' ) );
+        
+        // Solamente los usuarios con el rol de administrador podran entrar a la direccion http://<contextoDeGarcon>/movimientos/adquirir
+        $acl->allow('administrador', 'movimientos/adquirir');
+        
+        // ponemos el acl en Zend_Registry
+        Zend_Registry::set('acl',$acl);
+                
+        // Store ACL and role in the proxy helper
+        $view = $this->view;
+
+        // Si no se ha logueado, se asigna por default el rol de invitado
+        $autentificacion = Zend_Auth::getInstance();
+        $rol = 'invitado';
+        if ( $autentificacion->hasIdentity() ) {
+            $rol = $autentificacion->getIdentity()->rol;
+        }
+        
+        $view->navigation()->setAcl($acl)->setRole( $rol );
+        
+        // registramos el plugin que autentifica si el usuario puede ver la url
+        $controller = Zend_Controller_Front::getInstance();     
+        $controller->registerPlugin( new Garcon_Plugins_AuthPlugin() );
     }
 
     // Menu
